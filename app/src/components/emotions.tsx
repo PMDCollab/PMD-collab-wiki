0a86ea53ed4d24bafd1b1a90339ee209969b4468
marius david <mariusdavid@laposte.net>
2022-04-23 16:43:50 +0200

    order portrait by emotion name

--- app/src/components/emotions.tsx
+++ app/src/components/emotions.tsx
@@ -2,7 +2,7 @@ import { CDN_URL } from "../types/enum"
 
 export default function Emotions(props: {infoKey: string, emotions:{[key: string]: boolean}}){
     return <div style={{display:'flex', flexWrap:'wrap'}}>
-    {Object.keys(props.emotions).map(k=>{
+    {Object.keys(props.emotions).sort().map(k=>{
        return <div key={k} className="my-container nes-container" style={{margin:'10px', marginLeft:'0px', display:'flex', flexFlow:'column', justifyContent:'space-around', alignItems:'center'}}>
            <img className="my-img" alt="" src={`${CDN_URL}/portrait/${props.infoKey}/${k}.png`}/>
            <p style={{fontSize: '0.6em', margin: '0px'}}>{k}</p>
