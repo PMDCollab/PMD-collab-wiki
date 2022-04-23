c83f6cab74d29e85aec2148dcea59f760ff793f6
marius david <mariusdavid@laposte.net>
2022-04-23 16:30:38 +0200

    Take as much space as needed to display the name in the PokemonCaroussel

--- app/src/components/pokemon-thumbnail.tsx
+++ app/src/components/pokemon-thumbnail.tsx
@@ -5,9 +5,9 @@ import { ITracker } from '../types/ITracker';
 export default function PokemonThumbnail(props: {info: ITracker, infoKey: string}){
     const image = props.info.portrait_files[Emotion.NORMAL] !== undefined ? <img className='my-img' alt='' src={`${CDN_URL}/portrait/${props.infoKey}/${Emotion.NORMAL}.png`}/> : <h1 style={{height:'80px', margin:'0px'}}>?</h1>;
     return <Link to={props.infoKey} className='my-link'>
-        <div className='my-container nes-container nes-pointer grow' style={{display:'flex', flexFlow:'column', justifyContent:'space-between', alignItems:'center', width: '100px', margin:'20px'}}>
+        <div className='my-container nes-container nes-pointer grow' style={{display:'flex', flexFlow:'column', justifyContent:'space-between', alignItems:'center', minWidth: '100px', margin:'20px'}}>
              {image}
-            <p style={{fontSize: '0.6em', margin: '0px'}}>{props.infoKey} {props.info.name}</p>
+            <p style={{fontSize: '0.6em', margin: '0px'}}>{props.infoKey}<br />{props.info.name}</p>
         </div>
     </Link>
 }
