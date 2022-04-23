3dabd4b7750d625d29325bd4adf1af70337ebd23
marius david <mariusdavid@laposte.net>
2022-04-23 16:37:33 +0200

    Use another portrait if the Normal portrait is unavailable

--- app/src/components/pokemon-thumbnail.tsx
+++ app/src/components/pokemon-thumbnail.tsx
@@ -3,7 +3,15 @@ import { CDN_URL, Emotion } from '../types/enum';
 import { ITracker } from '../types/ITracker';
 
 export default function PokemonThumbnail(props: {info: ITracker, infoKey: string}){
-    const image = props.info.portrait_files[Emotion.NORMAL] !== undefined ? <img className='my-img' alt='' src={`${CDN_URL}/portrait/${props.infoKey}/${Emotion.NORMAL}.png`}/> : <h1 style={{height:'80px', margin:'0px'}}>?</h1>;
+    let image = undefined;
+    if (props.info.portrait_files[Emotion.NORMAL] !== undefined) {
+        image = <img className='my-img' alt='' src={`${CDN_URL}/portrait/${props.infoKey}/${Emotion.NORMAL}.png`}/>;
+    } else if (Object.keys(props.info.portrait_files).length > 0) {
+        image = <img className='my-img' alt='' src={`${CDN_URL}/portrait/${props.infoKey}/${Object.keys(props.info.portrait_files)[0]}.png`}/>;
+    } else {
+        image = <h1 style={{height:'80px', margin:'0px'}}>?</h1>;
+    }
+
     return <Link to={props.infoKey} className='my-link'>
         <div className='my-container nes-container nes-pointer grow' style={{display:'flex', flexFlow:'column', justifyContent:'space-between', alignItems:'center', minWidth: '100px', margin:'20px'}}>
              {image}
