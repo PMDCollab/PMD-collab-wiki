850e98c6c4daee3e586f4a3f00c4288b193015d7
keldaan <arnaudgregoire92@gmail.com>
2022-10-08 08:48:33 +0200

    better buttons, with twitter

--- app/src/components/buttons.tsx
+++ app/src/components/buttons.tsx
@@ -1,11 +1,13 @@
 import AboutButton from "./about-button"
 import DiscordButton from "./discord-button"
+import TwitterButton from './twitter-button'
 import HomeButton from "./home-button"
 
 export default function Buttons(){
     return <div style={{display:'flex'}}>
         <HomeButton/>
         <DiscordButton/>
+        <TwitterButton/>
         <AboutButton/>
     </div>
 }
---
+++ app/src/components/twitter-button.tsx
@@ -0,0 +1,6 @@
+export default function Twitter(){
+    return <a style={{fontSize:'.8em'}} href = "https://twitter.com/PMD_Spritebot" target="_blank" className='my-btn nes-btn is-primary'>
+        <i className="nes-icon twitter is-small"></i>
+        Twitter
+        </a>
+}
