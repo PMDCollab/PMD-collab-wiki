52fddeb6e1479a7916740bb10b1c28d1a51265c1
keldaan <arnaudgregoire92@gmail.com>
2023-06-24 08:16:49 +0200

    added a cache service worker for every assets. Cache gets reset when assetsCommit change.

---
+++ public/sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = localStorage.getItem('assetsCommit');
+
+// Cache-first strategy
+const cacheFirst = (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cacheResponse) => {
+            return cacheResponse || fetch(event.request).then((networkResponse) => {
+                if(networkResponse.ok && networkResponse.status !== 206){ // do not cache errors or partial content
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        try {
+                            cache.put(event.request, networkResponse.clone());
+                        } catch(err){
+                            console.error(err)
+                            // ignore if could not be cached
+                        }
+                        return networkResponse
+                    })
+                }
+                return networkResponse;
+            })
+        })
+    )
+}
+
+async function clearObsoleteCaches() {
+    const cachesKeys = await caches.keys()
+    return Promise.all(
+        cachesKeys.filter(key => key !== CACHE_NAME)
+            .map(oldCache => caches.delete(oldCache))
+    )
+}
+
+self.addEventListener('fetch', async (event) => {
+    const url = event.request.url
+    if (event.request.method === 'GET' && (
+        url.includes("/SpriteCollab/")
+    )) cacheFirst(event)
+});
+
+self.addEventListener('install', function () {
+    self.skipWaiting(); // immediately activates this service worker
+})
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clearObsoleteCaches().then(() => self.clients.claim()));
+});
