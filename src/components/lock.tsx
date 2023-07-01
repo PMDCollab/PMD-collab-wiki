d574f0653db2446fc9453273d7520c6e30339c17
keldaan <arnaudgregoire92@gmail.com>
2023-07-01 08:54:59 +0200

    different icons for locked/unlocked

--- src/components/lock.tsx
+++ src/components/lock.tsx
@@ -1,17 +1,18 @@
-import LockIcon from "@mui/icons-material/Lock"
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined"
+import LockOpenIcon from "@mui/icons-material/LockOpen"
 import { Tooltip } from "@mui/material"
 
 export default function Lock(props: { locked: boolean }) {
   if (props.locked) {
     return (
       <Tooltip title="This is locked and requires special permissions to be updated">
-        <LockIcon color="error" fontSize="small" />
+        <LockOutlinedIcon color="error" fontSize="small" />
       </Tooltip>
     )
   } else {
     return (
       <Tooltip title="This is unlocked, update do not require special permissions">
-        <LockIcon color="success" fontSize="small" />
+        <LockOpenIcon color="success" fontSize="small" />
       </Tooltip>
     )
   }
