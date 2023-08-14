32a0dbcd1c4e24234b9bcccfd23a8117306347b9
keldaan <arnaudgregoire92@gmail.com>
2023-08-14 07:41:05 +0200

    fix the inversion

--- src/Home.tsx
+++ src/Home.tsx
@@ -19,23 +19,56 @@ import { Bar } from "./components/bar"
 import { Footer } from "./components/footer"
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore"
 
-export type ShowParameters = Record<string, {
-  state: [boolean, Dispatch<SetStateAction<boolean>>],
-  rankMethod?: RankMethod,
-  name: string
-}>
+export type ShowParameters = Record<
+  string,
+  {
+    state: [boolean, Dispatch<SetStateAction<boolean>>]
+    rankMethod?: RankMethod
+    name: string
+  }
+>
 export default function Home({ ids, meta }: { ids: number[]; meta: Meta }) {
   const [currentText, setCurrentText] = useState("")
   const [rankBy, setRankBy] = useState<RankMethod>(RankMethod.POKEDEX_NUMBER)
   const showParameters: ShowParameters = {
-    index: { state: useState<boolean>(false), rankMethod: RankMethod.POKEDEX_NUMBER, name: "Index" },
-    portraitAuthor: { state: useState<boolean>(false), rankMethod: RankMethod.PORTRAIT_AUTHOR, name: "Portrait Author" },
-    spriteAuthor: { state: useState<boolean>(false), rankMethod: RankMethod.SPRITE_AUTHOR, name: "Sprite Author" },
-    lastModification: { state: useState<boolean>(false), rankMethod: RankMethod.LAST_MODIFICATION, name: "Last Change" },
-    portraitBounty: { state: useState<boolean>(false), rankMethod: RankMethod.PORTRAIT_BOUNTY, name: "Portrait Bounty" },
-    spriteBounty: { state: useState<boolean>(false), rankMethod: RankMethod.SPRITE_BOUNTY, name: "Sprite Bounty" },
-    fullyFeaturedSprites: { state: useState<boolean>(false), name: "Fully-Featured Portraits" },
-    fullyFeaturedPortraits: { state: useState<boolean>(false), name: "Fully-Featured Sprites" },
+    index: {
+      state: useState<boolean>(false),
+      rankMethod: RankMethod.POKEDEX_NUMBER,
+      name: "Index"
+    },
+    portraitAuthor: {
+      state: useState<boolean>(false),
+      rankMethod: RankMethod.PORTRAIT_AUTHOR,
+      name: "Portrait Author"
+    },
+    spriteAuthor: {
+      state: useState<boolean>(false),
+      rankMethod: RankMethod.SPRITE_AUTHOR,
+      name: "Sprite Author"
+    },
+    lastModification: {
+      state: useState<boolean>(false),
+      rankMethod: RankMethod.LAST_MODIFICATION,
+      name: "Last Change"
+    },
+    portraitBounty: {
+      state: useState<boolean>(false),
+      rankMethod: RankMethod.PORTRAIT_BOUNTY,
+      name: "Portrait Bounty"
+    },
+    spriteBounty: {
+      state: useState<boolean>(false),
+      rankMethod: RankMethod.SPRITE_BOUNTY,
+      name: "Sprite Bounty"
+    },
+    fullyFeaturedSprites: {
+      state: useState<boolean>(false),
+      name: "Fully-Featured Sprites"
+    },
+    fullyFeaturedPortraits: {
+      state: useState<boolean>(false),
+      name: "Fully-Featured Portraits"
+    }
   }
   const theme = useTheme()
   const isMobile = useMediaQuery(theme.breakpoints.down("md"))
@@ -50,8 +83,8 @@ export default function Home({ ids, meta }: { ids: number[]; meta: Meta }) {
           color="text.secondary"
           gutterBottom
         >
-          Free to use <strong>WITH CREDIT</strong> for ROMhacks, fangames, etc. Don't use for
-          commercial projects.
+          Free to use <strong>WITH CREDIT</strong> for ROMhacks, fangames, etc.
+          Don't use for commercial projects.
         </Typography>
         <Container maxWidth="md" sx={{ mt: 4, mb: 4 }}>
           <Typography
