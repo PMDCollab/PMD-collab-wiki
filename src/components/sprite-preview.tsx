f2e545b8f9ed987434f6b651c8ec3beae493b56f
keldaan <arnaudgregoire92@gmail.com>
2024-03-08 16:13:52 +0100

    Revert "add warnings"
    This reverts commit d14c557c1f23307131348f7723b4f18a26f45a3e.
    
    Revert "remove clearTextureCache"
    
    This reverts commit 4e56752dd42e1036ad33440f215c26f640e2b394.
    
    Revert "fix crash"
    
    This reverts commit 11c90c4b65179f98135a2fe7972fba62f86f2877.
    
    Revert "custom hooks"
    
    This reverts commit 57c9b242a70a922ddfe56e342b76e97f8a5bb276.
    
    Revert "refresh"
    
    This reverts commit 351603c43b68992166fb510442797b0d0694ca0c.
    
    Revert "change from phaser to react pixi"
    
    This reverts commit 76f92f8a647597f780ca1ebea9d4c506e0f22eee.

--- src/components/sprite-preview.tsx
+++ src/components/sprite-preview.tsx
@@ -1,29 +1,47 @@
-import { Card, Grid, Typography } from "@mui/material"
+import { useCallback, useRef } from "react"
 import { MonsterHistory, Sprite } from "../generated/graphql"
-import { Dungeon, IAnimData } from "../types/enum"
+import { Dungeon, IPMDCollab } from "../types/enum"
 import Lock from "./lock"
-import { SpriteContainer } from "./sprite-container"
+import GameContainer from "./phaser/game-container"
+import { Card, Grid, Typography } from "@mui/material"
+import { XMLParser } from 'fast-xml-parser'
 
 interface Props {
   sprite: Sprite
   dungeon: Dungeon
-  animationData: IAnimData
+  animDataXml: string
   history: MonsterHistory[]
-  infoKey: number
+  phaserWindows: GameContainer[]
 }
-export default function SpritePreview({
-  sprite,
-  dungeon,
-  animationData,
-  history
-}: Props) {
+export default function SpritePreview({ sprite, dungeon, animDataXml, history, phaserWindows }: Props) {
+  const gameContainer = useRef<GameContainer>()
+
+  const container = useCallback(
+    (node: HTMLDivElement) => {
+      async function initialize() {
+        const xmlData = await (await fetch(animDataXml)).text();
+        const parser = new XMLParser();
+        const data = parser.parse(xmlData) as IPMDCollab;
+        gameContainer.current = new GameContainer(
+          node,
+          sprite,
+          data.AnimData,
+          dungeon
+        )
+        phaserWindows.push(gameContainer.current);
+      }
+
+      if (node !== null) {
+        gameContainer.current?.game.destroy(true)
+        initialize()
+      }
+    },
+    [animDataXml, sprite, dungeon]
+  )
+
   return (
     <Card>
-      <SpriteContainer
-        dungeon={dungeon}
-        animationData={animationData}
-        sprite={sprite}
-      />
+      <div id={`action-${sprite.action}`} ref={container}></div>
       <Grid container justifyContent="center" alignItems="start">
         <Lock
           locked={sprite.locked}
