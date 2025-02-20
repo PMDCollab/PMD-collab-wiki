7ce3afc7bec36db8757246b2d704e36f9a6dd1fc
25Pi25 <25pi25@mail.com>
2025-02-20 14:40:26 -0500

    formatted file

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,7 +1,7 @@
 import { Box, Typography, Link } from '@mui/material';
 
 export default function ErrorPage() {
-  return <Box textAlign='center' alignItems='center' sx={{background: "rgba(255,255,255,0.8)"}}>
+  return <Box textAlign='center' alignItems='center' sx={{ background: "rgba(255,255,255,0.8)" }}>
     <h1>Uh Oh!</h1>
     <Typography>
       Looks like the server ran into an error. This typically happens when our sprite server is down, and we'll try to get it back up as soon as possible.
