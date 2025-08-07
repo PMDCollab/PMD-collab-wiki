27206ba9a7237b351e61050e5df1b768badeb1ba
25Pi25 <25pi25mail@gmail.com>
2025-08-07 00:48:06 -0700

    moved credits explanation to search parameters

--- src/About.tsx
+++ src/About.tsx
@@ -40,7 +40,7 @@ export default function About() {
           Terms of Use
         </Typography>
         <Typography>
-          All custom graphics in this repository not originating from official PMD games are licensed under <Link href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International</Link>. Usage of the assets in this repository is subject to the terms of the license.
+          All custom graphics in this repository not originating from official PMD games are licensed under <Link href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 International</Link>. Usage of the assets in this repository is subject to the terms of the license.           Custom sprites are licensed under <Link href="https://github.com/PMDCollab/SpriteCollab/blob/master/spritebot_credits.txt">spritebot_credits.txt</Link>, which contains all authors of the project.
         </Typography>
         <Container sx={{ mb: 2, mt: 2 }}>
           <Grid container spacing={2} justifyContent="space-around">
@@ -144,12 +144,6 @@ export default function About() {
           If you wish to use them, you must list all the authors that contributed to each asset you use, separately.
           The recommended way of doing this is enabling credits mode on the main page, selecting all the assets you used, and clicking the "download credits" button to generate a single file with all the required credits.
           Alternatively, each asset page contains a link to a credits.txt file with its credits. This file already follows the required format, so you can take the credits from there and then combine them for all the assets you use.
-        </Typography>
-        <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
-          Credits Mode
-        </Typography>
-        <Typography>
-          Credits Mode allows you to select multiple Pokémon and generate a single credits file with the corresponding credits for all of them. If you are selecting specific variants of a Pokémon, also enable the option to include different forms.
         </Typography>
 
         <Typography variant="h5" gutterBottom sx={{ mt: 2 }}>
